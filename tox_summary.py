6bedd4593299c23c8c83054ca500c12e1fd66bd7
Federico Ceratto <federico.ceratto@gmail.com>
2015-04-13 20:00:46 +0100

    Improve reporting

--- tox_summary.py
+++ tox_summary.py
@@ -8,6 +8,8 @@
 # Copyright Federico Ceratto <federico.ceratto@gmail.com>
 # Released under AGPLv3
 
+from argparse import ArgumentParser
+from collections import namedtuple
 from glob import glob
 import xmltodict
 
@@ -19,16 +21,20 @@ class Colors(object):
     FAIL = u'\033[91m'
     OK = u'\033[32m'
     WARNING = u'\033[93m'
-    BLUE = u'\033[94m'
+    SKIP = u'\033[94m'
     ENDC = u'\033[0m'
+
+
+Env = namedtuple('Env', 'all failed skipped')
 
 
 def extract_tests(fn):
     """Extract failed tests from result.xml
-    Return sets of all/failed (classname, name)
+    Return sets of all/failed/skipped (classname, name)
     """
     failed = set()
     all_tests = set()
+    skipped = set()
     with open(fn) as f:
         r = xmltodict.parse(f)
 
@@ -38,41 +44,62 @@ def extract_tests(fn):
             all_tests.add((classname, name))
             if u'failure' in t:
                 failed.add((classname, name))
+            elif u'skipped' in t:
+                skipped.add((classname, name))
 
-    return all_tests, failed
+    return Env(all_tests, failed, skipped)
+
+
+def parse_args():
+    ap = ArgumentParser()
+    ap.add_argument('-a', '--print-all', action='store_true',
+                    help="Print all tests")
+    return ap.parse_args()
 
 
 def main():
+    args = parse_args()
+
+    tests_to_be_printed = set()
     env_tests = {}
+
     for fn in glob(RESULT_GLOB):
         env_name = fn[7:][:-15]
-        env_tests[env_name] = extract_tests(fn)
+        results = extract_tests(fn)
+        env_tests[env_name] = results
+        if args.print_all:
+            tests_to_be_printed |= results.all
 
-    all_tests_li, failed_tests_li = zip(*env_tests.values())
-    all_failed_tests = reduce(set.union, failed_tests_li)
+        else:
+            tests_to_be_printed |= results.failed
 
-    if not all_failed_tests:
-        print "No failures."
+    if not tests_to_be_printed:
+        print "No tests found." if args.print_all else "No failures."
         return
 
     print
     print ' '.join(sorted(env_tests))
     print
 
-    for test in sorted(all_failed_tests):
+    for test in sorted(tests_to_be_printed):
         line = u"%s %s" % test
         line = u"%-90s" % line
         for env_name in sorted(env_tests):
-            failed = test in env_tests[env_name][1]
-            if failed:
+            results = env_tests[env_name]
+
+            if test in results.failed:
                 line += Colors.FAIL + SYMBOL
                 continue
 
-            found = test in env_tests[env_name][0]
-            if found:
+            if test in results.skipped:
+                line += Colors.SKIP + SYMBOL
+                continue
+
+            if test in results.all:
                 line += Colors.OK + SYMBOL
-            else:
-                line += Colors.WARNING + SYMBOL
+                continue
+
+            line += Colors.WARNING + SYMBOL
 
         line += Colors.ENDC
         print line
